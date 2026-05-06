f1e6d150b75bf48b272e31cf15e4a88fbba0da7f
Aryan Gupta <sanataniaryan010@gmail.com>
2026-05-06 06:10:35 +0530

    fix: add .onrender.com wildcard to ALLOWED_HOSTS

--- scheduler_project/settings.py
+++ scheduler_project/settings.py
@@ -9,6 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-produc
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+ALLOWED_HOSTS += ['.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
